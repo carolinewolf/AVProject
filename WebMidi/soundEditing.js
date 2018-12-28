b8aa032ace69e9a65896edd2fc81239417291dbf
carolinewolf <27232948+carolinewolf@users.noreply.github.com>
2018-12-28 14:34:39 +0100

    techn. Doku Anpassung 2

--- WebMidi/soundEditing.js
+++ WebMidi/soundEditing.js
@@ -101,8 +101,6 @@ function makeDistortionCurve(amount) {
 	var n_samples = 44100,
 		curve = new Float32Array(n_samples);
 
-	var test = [];
-
 	for (i = 0; i < n_samples; i++) {
 		var x = i * 2 / n_samples - 1;
 		curve[i] = (Math.PI + amount) * x / (Math.PI + (amount * Math.abs(x)));
